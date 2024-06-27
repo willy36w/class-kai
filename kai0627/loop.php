7de1415eb1cd22332e70a160125d8ecf202ea75e
kai~ <test@gmail.com>
2024-06-27 16:33:32 +0800

    光凱老師js_array&js_table

---
+++ kai0627/loop.php
@@ -0,0 +1,20 @@
+ <?php
+    for ($i = 1; $i <= 10; $i++) {
+        echo $i;
+    }
+
+    echo "<br>";
+    $i = 0;
+    while ($i < 10) {
+        $i++;
+        echo $i;
+    }
+
+    echo "<br>";
+    $i = 0;
+    do {
+        $i++;
+    } while ($i <= 10);
+    echo $i;
+
+    echo "<br>";
